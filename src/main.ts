7a3bb63dc110f4699796d2536205ce1e29274f8e
GoodLuckMister <paet14@gmail.com>
2021-11-16 17:25:15 +0200

    start setting passport

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   SwaggerModule.setup('/api/docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(process.env.PORT || 8080);
+  app.enableCors();
+  await app.listen(8080);
 }
 bootstrap();
