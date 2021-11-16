0df5f1de8143363cf4eb0b90f7e98c481044eed1
Daria <daria.kuliashova@gmail.com>
2021-11-16 16:21:18 +0100

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'app',
+    script: 'yarn',
+    args: 'start:dev',
+    // watch: true,
+    env: {
+      PORT: 8080,
+      DB_HOST: 'localhost',
+      DB_USER: 'root',
+      DB_PASSWORD: '',
+      DB_NAME: 'new_holidays',
+      DB_PORT: 3306,
+      SECRET_KEY: 'secret_key_safasf',
+    }
+  }]
+}
