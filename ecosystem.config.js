26509b5a046baddd8ba7219b940119a47780e52a
paet <paet14@gmail.com>
2021-11-18 12:39:24 +0200

    support pm2 local and cloud database

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,20 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
-  apps: [{
-    name: 'app',
-    script: 'yarn',
-    args: 'start:dev',
-    interpreter: "/bin/bash",
-    env: {
-      PORT: 8080,
-      DB_HOST: 'localhost',
-      DB_USER: 'root',
-      DB_PASSWORD: '',
-      DB_NAME: 'new_holidays',
-      DB_PORT: 3306,
-      SECRET_KEY: 'secret_key_safasf',
-    }
-  }]
-}
+  apps: [
+    {
+      name: 'app',
+      script: 'yarn',
+      args: 'start:dev',
+      interpreter: '/bin/bash',
+      env: {
+        PORT: 8080,
+        DB_HOST: 'remotemysql.com',
+        DB_USER: 'AIqO6told3',
+        DB_PASSWORD: 'ffQRNsZoNk',
+        DB_NAME: 'AIqO6told3',
+        DB_PORT: 3306,
+        SECRET_KEY: 'secret_key_safasf',
+      },
+    },
+  ],
+};
