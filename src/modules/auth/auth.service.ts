00e511396fab50286275d1c7ca61fbd3b1f7dbda
Alexey <paet14@gmail.com>
2021-11-19 19:10:51 +0200

    add session in auth service'

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -5,20 +5,66 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/sequelize';
 import { CreateUserDto, LoginUserDto } from 'modules/users/create-user.dto';
 import { UsersService } from 'modules/users/users.service';
 import { User } from 'models/users.model';
+import { Session } from 'models/session.model';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
+    @InjectModel(Session)
+    @InjectModel(User)
     private userService: UsersService,
     private jwtService: JwtService,
   ) {}
   async login(userDto: LoginUserDto) {
     try {
-      const User = await this.validateUser(userDto);
+      const LoginUser = await this.validateUser(userDto);
+      try {
+        const newSession = await Session.create({
+          uid: LoginUser.id,
+        });
+        const accessToken = this.jwtService.sign(
+          { uid: LoginUser.id, sid: newSession.id },
+          process.env.SECRET_KEY,
+          {
+            expiresIn: '1h',
+          },
+        );
+        const refreshToken = this.jwtService.sign(
+          { uid: LoginUser.id, sid: newSession.id },
+          process.env.SECRET_KEY,
+          {
+            expiresIn: '2d',
+          },
+        );
+        return UserSchema.findOne({ email }).exec((err, data) => {
+          if (err) {
+            next(err);
+          }
+          return res.status(httpCode.OK).send({
+            status: 'success',
+            code: httpCode.OK,
+            data: {
+              headers: {
+                accessToken,
+                refreshToken,
+                sid: newSession._id,
+              },
+              email: data.email,
+              name: data.name,
+              picture: data.picture,
+              id: data._id,
+              createdAt: data.createdAt,
+            },
+          });
+        });
+      } catch (e) {
+        console.log(e);
+      }
       const token = await this.generateToken(User);
       const user = {
         email: User.email,
